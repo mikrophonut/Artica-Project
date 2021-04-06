923688ae663cea8ade9591a0e5fe95605612c6af
stanislava-zheleva <81319637+stanislava-zheleva@users.noreply.github.com>
2021-04-07 02:30:02 +0300

    Portfolio JS added.

--- assets/js/zh_app.js
+++ assets/js/zh_app.js
@@ -1,22 +1,41 @@
-// Expand-info smaller device
+// Expand-info
 
-var holder = document.getElementsByClassName("zh_holder");
-var i;
+var item = document.getElementById("zh_item_1");
 
-for (i = 0; i < holder.length; i++) {
-  holder[i].addEventListener("click", function() {
-    this.classList.toggle("zh_active_small");
-  });
-}
+item.addEventListener("click", function () {
+    var expand = document.getElementById("zh_exp");
+    expand.classList.toggle("zh_active");
+});
+
+// Active item
+
+var item = document.getElementById("zh_item_1");
+
+item.addEventListener("click", function () {
+    this.classList.add("zh_active_item");
+});
+
 
 
 // Expand x-button
 
 var xclose = document.getElementById("zh_x");
 
-  xclose.addEventListener("click", function() {
-    
-	var content = document.getElementById("zh_exp");
-      content.style.display = "none";
-  });
+xclose.addEventListener("click", function () {
+    var expand = document.getElementById("zh_exp");
+  	expand.classList.remove("zh_active");	
+  	var item = document.getElementById("zh_item_1");
+  	item.classList.remove("zh_active_item");
+});
 
+
+// Expand-info smaller device
+
+var holder = document.getElementsByClassName("zh_holder");
+var i;
+
+for (i = 0; i < holder.length; i++) {
+    holder[i].addEventListener("click", function () {
+        this.classList.toggle("zh_active_small");
+    });
+}
